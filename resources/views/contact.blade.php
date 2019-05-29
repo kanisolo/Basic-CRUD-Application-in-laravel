43fed5260e44b30914f2b217598ec7f020728bbe
Solomon Kanihiro <solo2ballers@gmail.com>
2019-05-30 02:03:33 +0300

    Adding forms using laravel collective

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h3>Contact</h3>
+
+    <div class="col-md-6">
+        {{ Form::open(['url' => 'contact/submit']) }}
+            {{Form::label('name', 'Enter Name')}}
+            {{Form::text('name', '',["class"=>"form-control", "placeholder"=>"Enter Your Name"])}}
+        <br>
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::email('email', '',["class"=>"form-control", "placeholder"=>"Enter Your Email"])}}
+        <br>
+            {{Form::label('message', 'Message')}}
+            {{Form::textarea('message', '',["class"=>"form-control", "placeholder"=>"Enter Message"])}}
+        <br>
+            {{Form::submit('Send Message',["class"=>"btn btn-block btn-primary btn-lg"])}}
+        {{ Form::close() }}
+    </div>
+
 @endsection
