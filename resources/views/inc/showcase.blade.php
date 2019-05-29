b43a5accf2c7856b147a84796d41fe2a4facfa8a
Solomon Kanihiro <solo2ballers@gmail.com>
2019-05-30 01:20:22 +0300

    Used scss to compile assets using bootstrap. Added include files & nave bar

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4 text-center">My basic laravel Website</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aut cum eaque eveniet excepturi fugit nisi nostrum quis sunt.</p>
+    </div>
+</div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,10 +6,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Basic Website</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+    @include('inc.navbar')
 
-    @yield('content')
+    <div class="container">
+        @if(Request::is('/'))
+            @include('inc.showcase')
+        @endif
+
+        <div class="row">
+            <div class="col-md-12">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <footer id="footer" class="text-center">
+        Copyright {{date('Y')}} &copy; The basics
+    </footer>
+
 
 </body>
 </html>
