a1e5e410de46f9be5c898906057534808f30edc8
Solomon Kanihiro <solo2ballers@gmail.com>
2019-05-30 01:20:22 +0300

    Used scss to compile assets using bootstrap. Added include files & nave bar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h3>About</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem deserunt dolorum est et eum facere id illo in, incidunt ipsum libero molestiae neque numquam odio praesentium quod repellat tempora temporibus totam? Aperiam consequatur culpa debitis dolor doloremque dolorum ea eligendi, exercitationem fuga id iste itaque iusto laudantium magni necessitatibus nulla pariatur quae quaerat quas quidem, quos rem tempora ut!</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h3>Home</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis, cumque iste pariatur quasi veritatis vero? Alias, ducimus ipsum nostrum odit officia optio quibusdam voluptatibus. Assumenda cumque ea error in, minima natus quasi quidem sequi sunt temporibus. Nemo nihil quibusdam quos repellendus! Atque enim fuga nemo quam quis reiciendis repellat ullam. Expedita molestiae possimus quam rem temporibus ut voluptate. Reprehenderit.</p>
 @endsection
