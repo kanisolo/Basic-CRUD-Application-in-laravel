9dea4b7c2fb1bb6381e125a4f9943663791b63ea
Solomon Kanihiro <solo2ballers@gmail.com>
2019-05-30 11:16:07 +0300

    Models & migrations added to project

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -13,6 +14,17 @@ class MessagesController extends Controller
             'email' => 'required'
         ]);
 
-        return 'SUCCESS';
+        //Create New Message
+
+        $message = new Message;
+
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+
+        //Save the message
+        $message->save();
+
+        return redirect('/')->with('success','Your message has been sent');
     }
 }
